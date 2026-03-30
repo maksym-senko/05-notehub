e7c19d0f1fabcfed2d34fbb3b5167a91102d78d0
Maksym <maksskazka1997@gmail.com>
2026-03-30 14:29:03 +0200

    fix note

--- src/types/note.ts
+++ src/types/note.ts
@@ -1,4 +1,4 @@
-export type NoteTag = "Todo" | "Work" | "Personal" | "Meeting" | "Shopping";
+export type NoteTag = 'Todo' | 'Work' | 'Personal' | 'Meeting' | 'Shopping';
 
 export interface Note {
   id: string;
@@ -6,4 +6,5 @@ export interface Note {
   content: string;
   tag: NoteTag;
   createdAt: string;
+  updatedAt: string;
 }
