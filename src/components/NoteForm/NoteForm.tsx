15a61a7a60be916d603c411c23cfb3feb52b5528
Maksym <maksskazka1997@gmail.com>
2026-03-30 14:44:04 +0200

    final fix

--- src/components/NoteForm/NoteForm.tsx
+++ src/components/NoteForm/NoteForm.tsx
@@ -33,9 +33,35 @@ const NoteForm = ({ onCancel }: NoteFormProps) => {
       onSubmit={(values) => mutate(values)}
     >
       <Form className={css.form}>
+        <div className={css.fieldWrapper}>
+          <label htmlFor="title">Title</label>
+          <Field name="title" id="title" className={css.input} />
+          <ErrorMessage name="title" component="div" className={css.error} />
+        </div>
+
+        <div className={css.fieldWrapper}>
+          <label htmlFor="content">Content</label>
+          <Field as="textarea" name="content" id="content" className={css.textarea} />
+          <ErrorMessage name="content" component="div" className={css.error} />
+        </div>
+
+        <div className={css.fieldWrapper}>
+          <label htmlFor="tag">Tag</label>
+          <Field as="select" name="tag" id="tag" className={css.select}>
+            <option value="Todo">Todo</option>
+            <option value="Work">Work</option>
+            <option value="Personal">Personal</option>
+            <option value="Meeting">Meeting</option>
+            <option value="Shopping">Shopping</option>
+          </Field>
+          <ErrorMessage name="tag" component="div" className={css.error} />
+        </div>
+
         <div className={css.actions}>
-          <button type="button" onClick={onCancel}>Cancel</button>
-          <button type="submit" disabled={isPending}>
+          <button type="button" onClick={onCancel} className={css.cancelBtn}>
+            Cancel
+          </button>
+          <button type="submit" disabled={isPending} className={css.submitBtn}>
             {isPending ? 'Creating...' : 'Create note'}
           </button>
         </div>
